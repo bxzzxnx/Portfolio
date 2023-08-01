197e26da27671e03c5d105c3a774ca73fa5bd992
Antonio <antonio.bozzano3@hotmail.com>
2023-08-01 19:55:07 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,13 +1,21 @@
 export const data = [
   {
     id: 1,
+    name: 'Fastify API',
+    image:
+      'https://pbs.twimg.com/profile_images/970652657231847424/mWKpZoM4_400x400.jpg',
+    github: 'https://github.com/bxzzxnx/Fastify-API',
+  },
+
+  {
+    id: 2,
     name: 'Portfolio',
     image:
       'https://raw.githubusercontent.com/bxzzxnx/Portfolio/main/img/demo.png',
     github: 'https://github.com/bxzzxnx/Portfolio',
   },
   {
-    id: 2,
+    id: 3,
     name: 'React-To-Do-App',
     image:
       'https://raw.githubusercontent.com/bxzzxnx/To-Do-App/main/img/demo.jpg',
@@ -15,18 +23,10 @@ export const data = [
   },
 
   {
-    id: 3,
+    id: 4,
     name: 'Coversion app',
     image:
       'https://raw.githubusercontent.com/bxzzxnx/conversion-app/main/img/pc.png',
     github: 'https://github.com/bxzzxnx/conversion-app',
-  },
-
-  {
-    id: 4,
-    name: 'Fastify API',
-    image:
-      'https://pbs.twimg.com/profile_images/970652657231847424/mWKpZoM4_400x400.jpg',
-    github: 'https://github.com/bxzzxnx/Fastify-API',
   },
 ]
